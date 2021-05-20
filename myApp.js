058d24701aeec869429a60add1e597363ae614bc
AzizBooker <bookeraziz@gmail.com>
2021-05-20 08:49:17 -0400

    finshed serve html file and json get

--- myApp.js
+++ myApp.js
@@ -6,6 +6,9 @@ console.log('Hello World')
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/views/index.html')
 })
+app.get('/json',(req,res)=>{
+    res.json({"message": "Hello json"})
+})
 app.post('/name',(req,res)=>{
     res.send('name')
 })
