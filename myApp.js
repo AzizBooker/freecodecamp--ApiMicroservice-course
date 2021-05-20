8148fd55eb7cee1a3af7a6191c01984fa9c458dd
AzizBooker <bookeraziz@gmail.com>
2021-05-20 08:30:53 -0400

    first commit

---
+++ myApp.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+console.log('Hello World')
+
+app.listen(5000)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
